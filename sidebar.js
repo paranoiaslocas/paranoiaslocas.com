bbc5bfd67a23fc7a264f611507d18cfee005a97b
asd <ads@asd.com>
2021-07-31 21:09:42 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,3 @@
-
 var x = window.matchMedia("(max-width: 640px)")
 
 if (x.matches) {
@@ -17,7 +16,7 @@ if (x.matches) {
 
   function w3_open() {
       // document.getElementById("main").style.marginLeft = "25%";
-      document.getElementById("sidenav").style.width = "25%";
+      document.getElementById("sidenav").style.width = "100%";
       document.getElementById("sidenav").style.display = "block";
       // document.getElementById("openNav").style.display = 'none';
     }
