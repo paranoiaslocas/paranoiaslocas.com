f7fe15980f591f9135f27ebc65efca37c752952a
asd <ads@asd.com>
2021-07-31 21:12:36 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,5 @@
-var x = window.matchMedia("(max-width: 640px)")
 
-if (x.matches) {
-  function w3_open() {
+function w3_open() {
     // document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("sidenav").style.width = "100%";
     document.getElementById("sidenav").style.display = "block";
@@ -12,17 +10,3 @@ if (x.matches) {
     document.getElementById("sidenav").style.display = "none";
     // document.getElementById("openNav").style.display = "inline-block";
   }
-} else {
-
-  function w3_open() {
-      // document.getElementById("main").style.marginLeft = "25%";
-      document.getElementById("sidenav").style.width = "100%";
-      document.getElementById("sidenav").style.display = "block";
-      // document.getElementById("openNav").style.display = 'none';
-    }
-    function w3_close() {
-      // document.getElementById("main").style.marginLeft = "0%";
-      document.getElementById("sidenav").style.display = "none";
-      // document.getElementById("openNav").style.display = "inline-block";
-    }
-}
