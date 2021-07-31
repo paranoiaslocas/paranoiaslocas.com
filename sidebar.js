f387cbc5955e786aece01d5a12ef3e7cf51b7421
asd <ads@asd.com>
2021-07-31 20:55:10 +0200

    mbl upd, no audio yet

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,10 @@
-function w3_open() {
+
+var x = window.matchMedia("(max-width: 640px)")
+
+if (x.matches) {
+  function w3_open() {
     // document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("sidenav").style.width = "25%";
+    document.getElementById("sidenav").style.width = "100%";
     document.getElementById("sidenav").style.display = "block";
     // document.getElementById("openNav").style.display = 'none';
   }
@@ -9,3 +13,17 @@ function w3_open() {
     document.getElementById("sidenav").style.display = "none";
     // document.getElementById("openNav").style.display = "inline-block";
   }
+} else {
+
+  function w3_open() {
+      // document.getElementById("main").style.marginLeft = "25%";
+      document.getElementById("sidenav").style.width = "25%";
+      document.getElementById("sidenav").style.display = "block";
+      // document.getElementById("openNav").style.display = 'none';
+    }
+    function w3_close() {
+      // document.getElementById("main").style.marginLeft = "0%";
+      document.getElementById("sidenav").style.display = "none";
+      // document.getElementById("openNav").style.display = "inline-block";
+    }
+}
