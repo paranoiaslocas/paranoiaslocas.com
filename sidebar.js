f28f8df8aff18ca69221860d9ab1fc5319874004
Ignasi Nou <ignasinou@gmail.com>
2021-12-25 21:01:02 +0100

    update list

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,9 @@
 
 function w3_open() {
     // document.getElementById("main").style.marginLeft = "25%";
+    var user = firebase.auth().currentUser;
+
+    update_sound_list_user(user)
     document.getElementById("sidenav").style.width = "100%";
     document.getElementById("sidenav").style.display = "block";
     // document.getElementById("openNav").style.display = 'none';
